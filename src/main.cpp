d52c9d378fbbfcab9366f7cc942c2527815ee2f1
Feng <52739675+zzifer@users.noreply.github.com>
2023-12-14 17:06:49 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,13 +13,16 @@
 
 int main(int argc, char *argv[])
 {
+	// 定义cv::Mat类型的变量oImgFrm、oCamInParam和ovDistCoeff用于存储图像帧、相机内参和畸变系数
 	cv::Mat oImgFrm;
 	cv::Mat_<float> oCamInParam;
 	cv::Mat_<float> ovDistCoeff;
+	// 创建相机校准器CCamCal oCamCal和配置对象CCfg oCfg
 	CCamCal oCamCal;
 	CCfg oCfg;
 
 	// read configuration file
+	// 读取配置文件。如果参数数量超过2个，则显示用法信息并退出；否则，加载配置文件
 	if (2 < argc)
 	{
 		std::cout << "usage: " << argv[0] << " <cfg_file_path>" << std::endl;
@@ -34,6 +37,7 @@ int main(int argc, char *argv[])
 	oImgFrm = cv::imread(oCfg.getInFrmPth(), cv::IMREAD_COLOR);
 
 	// resize frame if necessary
+	// 如果配置中指定了调整帧大小，则对图像进行缩放
 	if (0 < oCfg.getRszFrmHei())
 	{
 		cv::Size oFrmSz((((float)oImgFrm.size().width / (float)oImgFrm.size().height) * oCfg.getRszFrmHei()), oCfg.getRszFrmHei());
@@ -41,6 +45,7 @@ int main(int argc, char *argv[])
 	}
 
 	// correct camera distortion
+	// 如果配置中指定了校正相机畸变，使用opencv的undistort函数进行图像畸变校正
 	if (oCfg.getCalDistFlg())
 	{
 		cv::Mat oImgUndist;
@@ -51,12 +56,15 @@ int main(int argc, char *argv[])
 	}
 
 	// initialize the camera calibrator
+	// 使用配置和读取的图像帧初始化相机校准器
 	oCamCal.initialize(oCfg, oImgFrm);
 
 	// run camera calibration
+	// 运行相机校准过程
 	oCamCal.process();
 
 	// output calibration results
+	// 输出校准结果
 	oCamCal.output();
 
 	return 0;
